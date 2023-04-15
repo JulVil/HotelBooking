a3ee50dfe139df760dda5076a81e28d65d07430d
JulVil <120428119+JulVil@users.noreply.github.com>
2023-04-15 20:07:53 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -28,7 +28,7 @@ mongoose.connection.on('disconnected', () => {
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: 'http://http://localhost:3000/',
+  origin: 'http://localhost:3000/',
   credentials: true
 }));
 
